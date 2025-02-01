b08c17920a50b9a5ee6009d1e69754eff5c83c3c
mohammad <malja-fa@c2r6s7>
2025-02-01 09:11:29 +0300

    feat: add documentation for safe_printf function

--- src/routine.c
+++ src/routine.c
@@ -6,12 +6,18 @@
 /*   By: malja-fa <malja-fa@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 09:12:06 by malja-fa          #+#    #+#             */
-/*   Updated: 2025/02/01 09:09:53 by malja-fa         ###   ########.fr       */
+/*   Updated: 2025/02/01 09:11:18 by malja-fa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <philo.h>
 
+/**
+    * safe_printf - Prints a message to the standard output.
+    * @msg: The message to be printed.
+    * @printf_mutex: The mutex that locks the standard output.
+    * due to the data races caused by the threads when they write to the standard output. 
+ */
 void safe_printf(const char *msg, pthread_mutex_t *printf_mutex)
 {
     pthread_mutex_lock(printf_mutex);
